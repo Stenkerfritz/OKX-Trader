3fbb641c9ec09d5398652b3ffb3d4eec06a6c176
m.Schubert <m.schubert@bbw-fi.de>
2024-03-06 15:35:59 +0100

    Login eingefügt

--- main.py
+++ main.py
@@ -5,33 +5,34 @@
 # 5. Funktionen für den Zugriff auf die API
 # 6. Schleife um alles, was immer wieder ausgeführt werden soll
 
-
-#import okx.Account as Account
-#import okx.MarketData as MarketData
-#import okx.PublicData as PublicData
+import confic
+import okx.Account as Account
+import okx.MarketData as MarketData
+import okx.PublicData as PublicData
+import okx.SpreadTrading as SpreadTrading
 import funktion
 
 if __name__ == "__main__":
 #    print("Test")
 
 # API initialization
-#apikey = "YOUR_API_KEY"
-#secretkey = "YOUR_SECRET_KEY"
-#passphrase = "YOUR_PASSPHRASE"
+    apikey = confic.YOUR_API_KEY
+    secretkey = confic.YOUR_SECRET_KEY
+    passphrase = input("Passwort")
 
-#flag = "1"  # live trading: 0, demo trading: 1
-#marketDataAPI = MarketData.MarketAPI(flag=flag)
-#publicDataAPI = PublicData.PublicAPI(flag=flag)
+    flag = "1"  # live trading: 0, demo trading: 1
+    marketDataAPI = MarketData.MarketAPI(flag=flag)
+    publicDataAPI = PublicData.PublicAPI(flag=flag)
 
-#accountAPI = Account.AccountAPI(apikey, secretkey, passphrase, False, flag)
+    accountAPI = Account.AccountAPI(apikey, secretkey, passphrase, False, flag)
 
     start = funktion.startbetrag
     stufen = funktion.sicherheit
     quote = funktion.steigung
 
 # Get maximum buy/sell amount or open amount
-#result = accountAPI.get_max_order_size(
-#    instId="BTC-USDT",
-#    tdMode="isolated"
-#)
-#print(result)
+spreadAPI = SpreadTrading.SpreadTradingAPI(apikey, secretkey, passphrase, False, flag)
+
+# get tickers
+result = spreadAPI.get_ticker(sprdId="BTC-USDT")
+print(result)
