8da28a72db505032618690fd9b18cf6a480e5935
m.Schubert <m.schubert@bbw-fi.de>
2024-03-06 13:35:52 +0100

    Mainmethode und Eingabe hinzugefügt

---
+++ funktion.py
@@ -0,0 +1,4 @@
+
+startbetrag=input("Bitte geben Sie den Startbetrag ein!")
+sicherheit=input("Bitte geben Sie die maximalen Sicherheitsstufen ein!")
+steigung=input("Bitte geben Sie die Steigerungquote ein!")
