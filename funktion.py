9e87af35b0576e508462d276c20763a37289c58d
m.Schubert <m.schubert@bbw-fi.de>
2024-03-14 09:15:59 +0100

    kauf überarbeite

--- funktion.py
+++ funktion.py
@@ -1,4 +1,31 @@
+import config
+import main
+import okx.Account as Account
+import okx.Trade as Trade
+import datetime
 
-startbetrag=input("Bitte geben Sie den Startbetrag ein!")
-sicherheit=input("Bitte geben Sie die maximalen Sicherheitsstufen ein!")
-steigung=input("Bitte geben Sie die Steigerungquote ein!")
+
+
+
+timestamp = datetime.now().isoformat(timespec='milliseconds') + 'Z'
+
+def kauf(waehrung, budget, flag):
+    tradeAPI = Trade.TradeAPI(config.YOUR_API_KEY, config.YOUR_SECRET_KEY, config.YOUR_PASS, timestamp,flag=flag)
+    result =tradeAPI.place_order(instId="BTC-USDT", tdMode="cash", clOrdId="b15", side="buy", ordType="limit", px="19000", sz="0.01")
+    print(result)
+
+    if result["code"] == "0":
+        print("Successful order request，order_id = ", result["data"][0]["ordId"])
+    else:
+        print("Unsuccessful order request，error_code = ",
+              #result["data"][0]["sCode"], ", Error_message = ",
+              result)
+
+
+def verkauf():
+    print("verkaufen")
+
+def warten():
+    print("nix")
+
+
--- main.py
+++ main.py
@@ -13,10 +13,13 @@ import okx.SpreadTrading as SpreadTrading
 import funktion
 import json
 
-if __name__ == "__main__":
-#    print("Test")
 
-# API initialization
+
+
+if __name__ == "__main__":
+    #    print("Test")
+
+    # API initialization
     apikey = config.YOUR_API_KEY
     secretkey = config.YOUR_SECRET_KEY
     passphrase = config.YOUR_PASS
@@ -25,28 +28,40 @@ if __name__ == "__main__":
     marketDataAPI = MarketData.MarketAPI(flag=flag)
     publicDataAPI = PublicData.PublicAPI(flag=flag)
 
-    accountAPI = Account.AccountAPI(apikey, secretkey, passphrase, False, flag)
-
-    start = funktion.startbetrag
     # stufen = funktion.sicherheit
     # quote = funktion.steigung
 
 # Get maximum buy/sell amount or open amount
-# spreadAPI = SpreadTrading.SpreadTradingAPI(apikey, secretkey, passphrase, False, flag)
-marketDataAPI = MarketData.MarketAPI(flag=flag)
-# get tickers
-result = marketDataAPI.get_tickers(instType="SPOT")
-print(result["code"])
-print(result["msg"])
-print(result["data"])
-json.dump(result,open('api-resultat.json','w'),indent=4,sort_keys=True)
+    handelsBudget = float(input("Bitte geben Sie den Startbetrag ein!"))
+    sicherheit = input("Bitte geben Sie an wie oft nachgekauf werden darf!")
+    steigung = input("Bitte geben Sie die Steigerungquote ein!")
 
-result = marketDataAPI.get_ticker(instId="BTC-USDT")
-print(result["data"])
-handelsWaehrung = result["data"][0]["instId"]
-aktuellerKurs = result["data"][0]["last"]
-print(handelsWaehrung)
-print(aktuellerKurs)
+# spreadAPI = SpreadTrading.SpreadTradingAPI(apikey, secretkey, passphrase, False, flag)
+    marketDataAPI = MarketData.MarketAPI(flag=flag)
+# get tickers
+    result = marketDataAPI.get_tickers(instType="SPOT")
+    print(result["code"])
+    print(result["msg"])
+    print(result["data"])
+    json.dump(result,open('api-resultat.json','w'),indent=4,sort_keys=True)
+
+    result = marketDataAPI.get_ticker(instId="BTC-USDT")
+    handelsWaehrung = result["data"][0]["instId"]
+    handelsKurs = result["data"][0]["last"]
+    print(handelsWaehrung)
+    print(handelsKurs)
+
+    funktion.kauf(handelsWaehrung, handelsBudget, flag)
+
+
+
+
+
+
+
+
+
+
 
 
 
