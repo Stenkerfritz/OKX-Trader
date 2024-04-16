45d296f635128b525d63ab2f3a766933b8cd50e7
m.Schubert <m.schubert@bbw-fi.de>
2024-04-16 08:26:10 +0200

    Verkauf mit gewinn in BTC hinzugefügt

--- funktion.py
+++ funktion.py
@@ -1,4 +1,6 @@
 import config
+import okx
+import okx.MarketData as MarketData
 import main
 import okx.Account as Account
 import okx.Trade as Trade
@@ -20,8 +22,8 @@ def GET_KAUF(flag, budget):
         clOrdId="b15",
         side="buy",
         ordType="market",
-        px="",
-        sz=budget
+        px=budget,
+        sz=""
     )
     print(result)
 
@@ -33,7 +35,34 @@ def GET_KAUF(flag, budget):
             # ", Error_message = ",result["data"][0]["sMsg"])
 
 
-def verkauf():
+def verkaufBtc(flag, budget):
+    tradeAPI = Trade.TradeAPI(config.GET_API_KEY(flag), config.GET_SECRET_KEY(flag), config.GET_PASS(flag), False)
+    result = tradeAPI.place_order(
+        instId="BTC-USDT",
+        tdMode="cash",
+        side="sell",
+        ordType="market",
+        sz=budget
+    )
+    print(result)
+
+    if result["code"] == "0":
+        print("Successful order request，order_id = ", result["data"][0]["ordId"])
+        print(result)
+    else:
+        print("Unsuccessful order request，error_code = ", result)
+        # ", Error_message = ",result["data"][0]["sMsg"])
+
+
+        # Verkaufsorder erstellen
+
+
+
+    # Beispielaufruf
+    #verkaufen_btc_zu_usdt(menge_usdt=100)
+
+
+def verkaufUsdt():
     print("verkaufen")
 
 def warten():
